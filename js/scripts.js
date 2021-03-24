fc45f9ecad2ed739b2fbafc44b48e95a4cc95c89
TorchAblaze <tiffanygreathead@gmail.com>
2021-03-24 00:42:01 -0700

    Add all terms and classes, WIP: addding the rest of the definitions

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,72 @@ $(document).ready(function () {
     $(".op-term-showing").slideToggle();
     $(".op-term-hidden").slideToggle();
   });
+  $(".var").click(function () {
+    $(".var-term-showing").slideToggle();
+    $(".var-term-hidden").slideToggle();
+  });
+  $(".var-name").click(function () {
+    $(".var-name-term-showing").slideToggle();
+    $(".var-name-term-hidden").slideToggle();
+  });
+  $(".funct").click(function () {
+    $(".funct-term-showing").slideToggle();
+    $(".funct-term-hidden").slideToggle();
+  });
+  $(".met").click(function () {
+    $(".met-term-showing").slideToggle();
+    $(".met-term-hidden").slideToggle();
+  });
+  $(".arg").click(function () {
+    $(".arg-term-showing").slideToggle();
+    $(".arg-term-hidden").slideToggle();
+  });
+  $(".para").click(function () {
+    $(".para-term-showing").slideToggle();
+    $(".para-term-hidden").slideToggle();
+  });
+  $(".ret").click(function () {
+    $(".ret-term-showing").slideToggle();
+    $(".ret-term-hidden").slideToggle();
+  });
+  $(".cha").click(function () {
+    $(".cha-term-showing").slideToggle();
+    $(".cha-term-hidden").slideToggle();
+  });
+  $(".str").click(function () {
+    $(".str-term-showing").slideToggle();
+    $(".str-term-hidden").slideToggle();
+  });
+  $(".bool").click(function () {
+    $(".bool-term-showing").slideToggle();
+    $(".bool-term-hidden").slideToggle();
+  });
+  $(".und").click(function () {
+    $(".und-term-showing").slideToggle();
+    $(".und-term-hidden").slideToggle();
+  });
+  $(".nan").click(function () {
+    $(".nan-term-showing").slideToggle();
+    $(".nan-term-hidden").slideToggle();
+  });
+  $(".null").click(function () {
+    $(".null-term-showing").slideToggle();
+    $(".null-term-hidden").slideToggle();
+  });
+  $(".ale").click(function () {
+    $(".ale-term-showing").slideToggle();
+    $(".ale-term-hidden").slideToggle();
+  });
+  $(".com").click(function () {
+    $(".com-term-showing").slideToggle();
+    $(".com-term-hidden").slideToggle();
+  });
+  $(".jq").click(function () {
+    $(".jq-term-showing").slideToggle();
+    $(".jq-term-hidden").slideToggle();
+  });
+  $(".att").click(function () {
+    $(".att-term-showing").slideToggle();
+    $(".att-term-hidden").slideToggle();
+  });
 });
